4bbb4721e48193b74fb42ff3384519db609780f5
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-20 13:39:22 -0700

    Filled in lightMachine

--- src/state-machines/LightMachine.js
+++ src/state-machines/LightMachine.js
@@ -2,8 +2,20 @@ import { createMachine, interpret } from 'xstate';
 
 const lightMachine = createMachine({
   // Your state machine blueprint here
-
-});
+  id: 'toggle',
+  initial: 'red',
+  states: {
+    red :{
+      on: {changeLight: 'green'}
+      },
+    yellow:{
+      on: {changeLight: 'red'}
+    },
+    green:{
+      on: {changeLight: 'red'}
+    }
+  }}
+);
 
 
 const service = interpret(lightMachine);
